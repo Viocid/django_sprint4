1c512f72d5e960a2c661ed6938cbe849bceec003
Konstantin <153995261+Viocid@users.noreply.github.com>
2024-05-28 06:16:20 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Category, Location, Post
+from .models import Category, Location, Post, MyUser
+
+
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Post)
